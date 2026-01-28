3d8f20c82302430de6ebb60e47f78e481cba4d3d
L-Bre411 <laura.bremont@gmail.com>
2026-01-28 13:42:16 +0100

    Ajout des fichiers
    Ajout des fichiers depuis le répertoire personnel (aucune classification sécurité particulière)

---
+++ Duhamel_methodes.py
@@ -0,0 +1,12 @@
+#%%
+"""
+Méthodes de calcul de l'intégrae de Duhamel
+pour la production d'un spectre de réponse d'un oscillateur harmonique 
+
+Auteur : Laura Brémont (stagiaire)
+Dernière version : 26/01/26
+"""
+
+import math
+import numpy as np
+import matplotlib.pyplot as plt
